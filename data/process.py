b66af9ee46577cf45bad0c1e8ed8b994be388d11
Zhuoheng Li <40489953+Andy-LZH@users.noreply.github.com>
2024-11-19 00:09:00 -0700

    now all initialized with subparts and objects selected

--- data/process.py
+++ data/process.py
@@ -2,39 +2,36 @@ import json
 import os
 
 # Path to your JSON file
-data_file_paths = ["ivc-unambigous.json", "ivc-ambigous.json"]
+data_file_paths = ["ivc-unambigous", "ivc-ambigous"]
 
+# range is 0 to 50 with ivc-ambigous-0.json and ivc-unambigous-0.json
+
+# Iterate over each file
 for data_file_path in data_file_paths:
     # Read the JSON data
-    with open(data_file_path, "r", encoding="utf-8") as file:
-        data = json.load(file)
-        print(len(data))
+    for i in range(0, 51):
+        data_file_path_r = f"{data_file_path}-{i}.json"
+        print(data_file_path_r)
+        with open(data_file_path_r, "r", encoding="utf-8") as file:
+            data = json.load(file)
+            print(len(data))
 
-    # Iterate over each entry in the data
-    for entry in data:
-        # Check if 'questions' exists and 'original_questions' is missing, and add if needed
-        if "questions" in entry and "original_questions" not in entry:
-            entry["original_questions"] = entry["questions"][:]
+        # Iterate over each entry in the data
+        for entry in data:
+            length_of_subparts = len(entry["parts_polygons"]["polygons"])
+            length_of_objects = len(entry["objects_polygons"])
 
-        entry["selected_questions"] = []
+            entry["selected_objects_polygons"] = []
+            entry["selected_parts_polygons"] = []
 
-        # Check if 'imageURL' exists and starts with 'http'
-        if "imageURL" in entry and entry["imageURL"].startswith("http://"):
-            entry["imageURL"] = entry["imageURL"].replace("http://", "https://", 1)
+            for i in range(length_of_subparts):
+                entry["selected_parts_polygons"].append(i)
 
-        # check all entry start with selected_ and make them empty
-        for key in entry.keys():
-            if key.startswith("selected_"):
-                entry[key] = []
+            for i in range(length_of_objects):
+                entry["selected_objects_polygons"].append(i)
 
-    # split into files with 50 entries each
-    data_split = [data[i : i + 50] for i in range(0, len(data), 50)]
-    for i, data in enumerate(data_split):
-        with open(
-            f"{data_file_path.split('.')[0]}-{i}.json", "w", encoding="utf-8"
-        ) as file:
-            json.dump(data, file, indent=2, ensure_ascii=False)
+        # Write the updated data back to the file
+        with open(data_file_path_r, "w", encoding="utf-8") as file:
+            json.dump(data, file, indent=4)
 
-    print(
-        "Updated JSON file: added original_questions where missing and changed all imageURLs to start with https."
-    )
+        print(f"Processed file: {data_file_path}")
